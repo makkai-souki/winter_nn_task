a246d676b17812c1840908b65ed1fdfafc3092f2
Kai Matsui <kai0927dentetsu@gmail.com>
2019-12-23 10:22:46 +0900

    データセット作成の基本関数を作製

--- format/format_model.py
+++ format/format_model.py
@@ -14,31 +14,45 @@ class format_model:
             保存先のディレクトリ, by default 'data'
         """
         self.save_dir = save_dir
-        print(os.path.exists(self.save_dir))
 
-    def make_flames(self, video_name, label, *, dirpass="flames", base_name="flame"):
+    def make_frames(
+        self, video_name, label, *, dirpass="frames", base_name="frame", ext="jpg"
+    ):
         """ 動画をフレームに分割する変更する関数
         
         Parameters
         ----------
         video_name : string
             入力する動画の名称
-        label : int
+        label : str
             つけるラベル
         dirpass : str, optional
-            ディレクトリのパス, by default 'flames'
+            ディレクトリのパス, by default 'frames'
         base_name : str, optional
-            出力画像の名称, by default 'flame'
+            出力画像の名称, by default 'frame'
 
         Returns
         -------
-        out_flames : list
+        out_frames : list
             出力するフレームの配列. opencv形式3channelのnumpy配列のリスト
         """
         label_dir = self.save_dir + label
-        print(os.path.exists(label_dir))
+        os.makedirs(label_dir, exist_ok=True)
+        cap = cv2.VideoCapture(video_name)
+        if not cap.isOpened():
+            return -1
+        n = 0
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                cv2.imwrite(
+                    "{}/{}_{}.{}".format(label_dir, base_name, str(n), ext), frame
+                )
+                n += 1
+            else:
+                return 0
 
-    def grayscale(self, filename, *, mode=0, outname="self", out_mode=0):
+    def grayscale(self, *, filematrix=None, filename=None, outname="self", mode=0):
         """ グレースケール化の関数
         
         Parameters
@@ -52,19 +66,30 @@ class format_model:
         outname : str, optional
             アウトプットするファイル名（selfはfilenameを同様）
         outmode : int, optional
-            アウトプットの形式（0: ファイル化, 1: no.array）
+            アウトプットの形式（0: ファイル化, 1: np.array）
         
         Returns
         -------
         gray_image
         """
-        if filematrix is None or filename is None:
+        if filename is None and filematrix is None:
             return -1
-        if filename is not None:
-            pass
+        if filematrix is None:
+            out = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+            if outname == "self":
+                outname = self.save_dir + filename
+            else:
+                outname = self.save_dir + outname
+        else:
+            out = cv2.cvtColor(filematrix, cv2.COLOR_BGR2GRAY)
+        if mode == 0:
+            cv2.imwrite(outname, out)
+            return 0
+        else:
+            return out
 
-    def resize_flame(
-        self, *, filematrix=None, filename=None, outname="self", size=(50, 50)
+    def resize_frame(
+        self, *, filematrix=None, filename=None, outname="self", size=(50, 50), mode=0
     ):
         """ 画像をリサイズする関数
         
@@ -79,7 +104,21 @@ class format_model:
         size : tuple, optional
             画像サイズ, by default (50, 50)
         """
-        pass
+        if filename is None and filematrix is None:
+            return -1
+        if filename is not None:
+            filematrix = cv2.imread(filename)
+            if outname == "self":
+                outname = self.save_dir + filename
+            else:
+                outname = self.save_dir + outname
+        out = cv2.resize(filematrix, dsize=size)
+        if mode == 0:
+            print(outname)
+            cv2.imwrite(outname, out)
+            return 0
+        else:
+            return out
 
     def convert_zip(self, dir_name):
         """ フォルダを圧縮する
--- test.py
+++ test.py
@@ -2,6 +2,13 @@ from format.format_model import format_model
 import numpy as np
 import matplotlib.pyplot as plt
 
-if __name__ == "main":
-    fm = format_model()
+
+fm = format_model()
+# print(fm.make_frames("./video/IMG_0078.MOV", "test2/new"))
+print(
+    fm.resize_frame(
+        filename="./data/test2/new/frame_1.jpg", outname="resize.jpg", size=(100, 100)
+    )
+)
+print("hello world")
 

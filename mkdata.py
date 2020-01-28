6bf4f2dac05cf5df9a9ed49eb36e35f6f0acfd6b
Kai Matsui <kai0927dentetsu@gmail.com>
2020-01-28 16:20:55 +0900

    提出版

--- mkdata.py
+++ mkdata.py
@@ -3,28 +3,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 videos = {
-    "s1_1": "s",
-    "s1_2": "s",
-    "h1_2": "h",
-    "h1_3": "h",
-    "h2_1": "h",
-    "k1_1": "k",
-    "k1_2": "k",
-    "k1_3": "k",
-    "d1_1": "d",
-    "d1_2": "d",
-    #
-    "s2_1": "s",
-    "h1_1": "h",
-    "k2_1": "k",
-    "d2_1": "d",
+    "s1_1": "1",
+    "s2_1": "2",
+    "s3_1": "3",
+    "s4_1": "4",
+    "s5_1": "5",
+    "s6_1": "6",
+    "s7_1": "7",
+    "s7_2": "7",
+    "s8_1": "8",
+    "s9_1": "9",
+    "s10_1": "10",
+    "sj_1": "j",
+    "sq_1": "q",
+    "sk_1": "k",
+    "h1_1": "1",
+    "h2_1": "2",
+    "h3_1": "3",
+    "h4_1": "4",
+    "h5_1": "5",
+    "h6_1": "6",
+    "h7_1": "7",
+    "h7_2": "7",
+    "h8_1": "8",
+    "h9_1": "9",
+    "h10_1": "10",
+    "hj_1": "j",
+    "hq_1": "q",
+    "hk_1": "k",
 }
 print(videos.items())
 for key, value in videos.items():
     print("./video/" + key + ".MOV")
     fc = format_controller("./video/" + key + ".MOV", value)
-    fc.make_datasets()
-fc.fm.convert_markdata2npz(0.2)
+    fc.make_datasets(input_size=(50, 50))
+fc.fm.convert_markdata2npz(
+    0.2, labels=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"]
+)
 # fs = format_controller("./video/s1.MOV", "s")
 # fs.make_datasets()
 # fk = format_controller("./video/k1.MOV", "k")
--- nn/nn_controller.py
+++ nn/nn_controller.py
@@ -5,16 +5,51 @@ from nn.nn_model import nn_model
 
 
 class nn_controller:
-    def __init__(self, *, data_pass="./data/dataset.npz", epochs=20):
+    def __init__(
+        self, *, data_pass="./data/dataset.npz", epochs=20, input_size=(50, 50)
+    ):
         self.data_pass = data_pass
-        self.nn = nn_model(epochs=epochs)
+        self.nn = nn_model(
+            epochs=epochs,
+            categories=[
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "j",
+                "q",
+                "k",
+            ],
+        )
+        self.size = input_size
 
     def mark_predict(self):
         self.nn.load_data(data_pass=self.data_pass)
-        self.nn.define_mark_model()
+        self.nn.define_mark_model(size=self.size)
         self.nn.compile_model()
         self.nn.fitting()
 
+    def num_predict(self):
+        self.nn.load_data(data_pass=self.data_pass)
+        print("loaded")
+        self.nn.define_num_model(size=self.size)
+        print("defined")
+        self.nn.compile_model()
+        print("compiled")
+        self.nn.fitting()
+
     def get_results(self):
-        self.nn.get_lossepoch_graph()
+        self.nn.save_model()
         self.nn.get_accepoch_graph()
+        self.nn.get_lossepoch_graph()
+        self.nn.get_graph()
+        self.nn.get_predict_map()
+        self.nn.get_precision()
+        self.nn.get_recall()
+        self.nn.get_f_mean()
